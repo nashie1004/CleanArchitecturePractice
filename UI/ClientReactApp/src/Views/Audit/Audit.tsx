0772666f6206fc1ea413da7c813804f0463f8bce
Nash Andrew <112688217+nashie1004@users.noreply.github.com>
2024-12-13 09:00:29 +0800

    Add icons and minor table tweaks

--- UI/ClientReactApp/src/Views/Audit/Audit.tsx
+++ UI/ClientReactApp/src/Views/Audit/Audit.tsx
@@ -12,7 +12,10 @@ import {
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table";
-import React from "react";
+import { useState, useEffect } from "react";
+import AuditService from "../../Services/AuditService"
+import CIcon  from '@coreui/icons-react';
+import { cilMoodGood, cilNotes, cilPencil, cilPhone } from '@coreui/icons';
 
 const mockData = 
     [
@@ -41,18 +44,20 @@ const columns = [
     columnHelper.accessor("id", {
         cell: (info) => info.getValue(),
         header: () => (
-            <span>
-                ID
-            </span>
+            <div className="d-flex align-items-center">
+                <CIcon icon={cilMoodGood} size="lg"  />
+                <span>Id</span>
+            </div>
         ),
     }),
 
     columnHelper.accessor("name", {
         cell: (info) => info.getValue(),
         header: () => (
-            <span >
-                Name
-            </span>
+            <div className="d-flex align-items-center">
+                <CIcon icon={cilMoodGood} size="lg" />
+                <span>Name</span>
+            </div>
         ),
     }),
     columnHelper.accessor("email", {
@@ -61,25 +66,48 @@ const columns = [
             <span >{info.getValue()}</span>
         ),
         header: () => (
-            <span >
-                 Email
-            </span>
+            <div className="d-flex align-items-center">
+                <CIcon icon={cilNotes} size="lg" />
+                <span>Email</span>
+            </div>
         ),
     }),
     columnHelper.accessor("phone", {
         header: () => (
-            <span >
-                 Phone
-            </span>
+            <div className="d-flex align-items-center">
+                <CIcon icon={cilPhone} size="lg" />
+                <span>Phone</span>
+            </div>
         ),
         cell: (info) => info.getValue(),
     }),
 ];
 
+const auditService = new AuditService();
+
 export default function Audit() {
-    const [data] = React.useState(() => [...mockData]);
-    const [sorting, setSorting] = React.useState([]);
-    const [globalFilter, setGlobalFilter] = React.useState("");
+    const [data] = useState(() => [...mockData]);
+    const [sorting, setSorting] = useState([]);
+    const [globalFilter, setGlobalFilter] = useState("");
+
+    const [testData, setTestData] = useState<any>(null);
+
+    useEffect(() => {
+        async function getData() {
+            const res = await auditService.getMany({
+                pageNumber: 1,
+                pageSize: 5,
+                filters: "",
+                sortBy: ""
+            });
+            if (res.isOk) {
+                setTestData(res.data)
+                console.log(res)
+            }
+        }
+
+        //getData();
+    }, [])
 
     const table = useReactTable({
         data,
@@ -106,23 +134,21 @@ export default function Audit() {
 
     return (
         <>
-            <>
-                <CInputGroup className="mb-3">
-                    <CInputGroupText >Icon</CInputGroupText>
-                    <CFormInput placeholder="Search..." aria-label="Search" aria-describedby="basic-addon1" />
-                    <CDropdown alignment="end" variant="input-group">
-                        <CDropdownToggle color="primary" variant="outline">Dropdown</CDropdownToggle>
-                        <CDropdownMenu>
-                            <CDropdownItem href="#">Action</CDropdownItem>
-                            <CDropdownItem href="#">Another action</CDropdownItem>
-                            <CDropdownItem href="#">Something else here</CDropdownItem>
-                        </CDropdownMenu>
-                    </CDropdown>
-                    <CButton type="button" color="primary" variant="outline"  id="button-addon2">Search</CButton>
-                </CInputGroup>
-            </>
+            <CInputGroup className="mb-3">
+                <CInputGroupText >Icon</CInputGroupText>
+                <CFormInput placeholder="Search..." aria-label="Search" aria-describedby="basic-addon1" />
+                <CDropdown alignment="end" variant="input-group">
+                    <CDropdownToggle color="dark" >Dropdown</CDropdownToggle>
+                    <CDropdownMenu>
+                        <CDropdownItem href="#">Action</CDropdownItem>
+                        <CDropdownItem href="#">Another action</CDropdownItem>
+                        <CDropdownItem href="#">Something else here</CDropdownItem>
+                    </CDropdownMenu>
+                </CDropdown>
+                <CButton type="button" color="dark" id="button-addon2">Search</CButton>
+            </CInputGroup>
 
-            <CTable responsive >
+            <CTable responsive bordered>
                     <CTableHead>
                         {table.getHeaderGroups().map((headerGroup) => (
                             <CTableRow key={headerGroup.id}>
@@ -208,7 +234,7 @@ export default function Audit() {
                         <span aria-hidden="true">&raquo;</span>
                     </CPaginationItem>
                 </CPagination>
-
+            {/* 
                 <span >
                     <input
                         min={1}
@@ -222,181 +248,7 @@ export default function Audit() {
                     />
                     <span >of {table.getPageCount()}</span>
                 </span>
+            */}
         </>
     );
 }
-/*
-import * as React from 'react'
-
-import {
-    createColumnHelper,
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
-} from '@tanstack/react-table'
-import { CButton, CTable, CTableBody, CTableCaption, CTableDataCell, CTableHead, CTableRow } from '@coreui/react'
-
-type Person = {
-    firstName: string
-    lastName: string
-    age: number
-    visits: number
-    status: string
-    progress: number
-}
-
-const defaultData: Person[] = [
-    {
-        firstName: 'tanner',
-        lastName: 'linsley',
-        age: 24,
-        visits: 100,
-        status: 'In Relationship',
-        progress: 50,
-    },
-    {
-        firstName: 'tandy',
-        lastName: 'miller',
-        age: 40,
-        visits: 40,
-        status: 'Single',
-        progress: 80,
-    },
-    {
-        firstName: 'joe',
-        lastName: 'dirte',
-        age: 45,
-        visits: 20,
-        status: 'Complicated',
-        progress: 10,
-    },
-]
-
-const columnHelper = createColumnHelper<Person>()
-
-const columns = [
-    columnHelper.accessor('firstName', {
-        cell: info => info.getValue(),
-        footer: info => info.column.id,
-    }),
-    columnHelper.accessor(row => row.lastName, {
-        id: 'lastName',
-        cell: info => <i>{info.getValue()}</i>,
-        header: () => <span>Last Name</span>,
-        footer: info => info.column.id,
-    }),
-    columnHelper.accessor('age', {
-        header: () => 'Age',
-        cell: info => info.renderValue(),
-        footer: info => info.column.id,
-    }),
-    columnHelper.accessor('visits', {
-        header: () => <span>Visits</span>,
-        footer: info => info.column.id,
-    }),
-    columnHelper.accessor('status', {
-        header: 'Status',
-        footer: info => info.column.id,
-    }),
-    columnHelper.accessor('progress', {
-        header: 'Profile Progress',
-        footer: info => info.column.id,
-    }),
-]
-
-export default function Audit() {
-    const [data, _setData] = React.useState(() => [...defaultData])
-    const rerender = React.useReducer(() => ({}), {})[1]
-
-    const table = useReactTable({
-        data,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-    })
-
-    return (
-        <>
-            <CTable responsive striped >
-                <CTableCaption>List of users</CTableCaption>
-                <CTableHead>
-                    {table.getHeaderGroups().map(headerGroup => (
-                        <CTableRow key={headerGroup.id}>
-                            {headerGroup.headers.map(header => (
-                                <CTableDataCell key={header.id}>
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )}
-                                </CTableDataCell>
-                            ))}
-                        </CTableRow>
-                    ))}
-                </CTableHead>
-                <CTableBody>
-                    {table.getRowModel().rows.map(row => (
-                        <CTableRow key={row.id}>
-                            {row.getVisibleCells().map(cell => (
-                                <CTableDataCell key={cell.id}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                </CTableDataCell>
-                            ))}
-                        </CTableRow>
-                    ))}
-                </CTableBody>
-                <CTableHead>
-                    {table.getFooterGroups().map(footerGroup => (
-                        <CTableRow key={footerGroup.id}>
-                            {footerGroup.headers.map(header => (
-                                <CTableDataCell key={header.id}>
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                            header.column.columnDef.footer,
-                                            header.getContext()
-                                        )}
-                                </CTableDataCell>
-                            ))}
-                        </CTableRow>
-                    ))}
-                </CTableHead>
-            </CTable>
-            <div className="h-4" />
-            <CButton onClick={() => rerender()} className="border p-2">
-                Rerender
-            </CButton>
-        </>
-    )
-}
-*/
-
-/*
-
-export default function Audit() {
-    const [data, setData] = useState<any>(null);
-
-    useEffect(() => {
-        async function getData() {
-            const res = await authService.getMany({
-                pageNumber: 1,
-                pageSize: 5,
-                filters: "",
-                sortBy: ""
-            });
-            if (res.isOk) {
-                setData(res.data)
-                console.log(res)
-            }
-        }
-
-        //getData();
-    }, [])
-
-    return <>
-    <CSpinner/> 
-        dashboard component
-        {data !== null ? JSON.stringify(data) : "no data"}
-    </>
-}
-*/
