a64762ae8feadba931ea36fce4abc82b03fe788e
Nash <naocenar@gmail.com>
2024-12-15 21:11:44 +0800

    Next, Previous, Page Size btns ok

--- UI/ClientReactApp/src/Views/Audit/Audit.tsx
+++ UI/ClientReactApp/src/Views/Audit/Audit.tsx
@@ -1,121 +1,66 @@
-import  { useCallback, useMemo, useRef, useState } from "react";
-
+import  { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
-import data from "./testData.json"
 import useTheme from "../../Hooks/useTheme";
-import { CButton, CCol, CContainer, CFormInput, CFormSelect, CInputGroup, CInputGroupText, CProgress, CRow } from "@coreui/react";
+import { CButton, CCol, CContainer, CFormInput, CFormSelect, CInputGroup, CInputGroupText, CProgress, CRow, CSpinner } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
 import { cilCaretLeft, cilCaretRight, cilPlus, cilSearch } from "@coreui/icons";
+import AuditService from "../../Services/AuditService";
+import { toast } from "react-toastify";
 
-const rowSelection = {
-  mode: "multiRow",
-  headerCheckbox: false,
-};
-
-const oneData = 
-{
-  "make": "Tesla",
-  "model": "Model Y",
-  "price": 64950,
-  "electric": true,
-  "month": "June"
-};
+const auditService = new AuditService();
+const columns = [
+  { field: "auditId" },
+  { field: "oldData" },
+  { field: "newData" },
+  { field: "tablePrimaryKey" },
+  { field: "tableName" },
+  { field: "action" },
+  { field: "createdBy" },
+  { field: "createdDate" },
+  { field: "lastUpdatedBy" },
+  { field: "lastUpdatedDate" }
+]
 
 export default function Audit(){
   const {theme} = useTheme();
   const gridRef = useRef();
-  const [rowData, setRowData] = useState([])
+  const [columnDefs] = useState(columns);
+  const [tableState, setTableState] = useState({
+    pageSize: 15,
+    pageNumber: 1,
+    sortBy: "",
+    filters: "",
+    rowData: [],
+    isLoading: false
+  })
 
-  const [columnDefs, setColumnDefs] = useState([
-    {
-      field: "make",
-      editable: true,
-      cellEditor: "agSelectCellEditor",
-      cellEditorParams: {
-        values: [
-          "Tesla",
-          "Ford",
-          "Toyota",
-          "Mercedes",
-          "Fiat",
-          "Nissan",
-          "Vauxhall",
-          "Volvo",
-          "Jaguar",
-        ],
-      },
-    },
-    { field: "model" },
-    { field: "price", filter: "agNumberColumnFilter" },
-    { field: "electric" },
-    {
-      field: "month",
-      comparator: (valueA, valueB) => {
-        const months = [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ];
-        const idxA = months.indexOf(valueA);
-        const idxB = months.indexOf(valueB);
-        return idxA - idxB;
-      },
-    },
-  ]);
+  async function getData(){
+    setTableState(prev => ({ ...prev, isLoading: true}))
+    const data = await auditService.getMany(tableState);
+    
+    if (!data.isOk){
+      toast(data.message, { type: "error" })
+      return;
+    }
+
+    setTableState(prev => ({
+      ...prev,
+      isLoading: false,
+      rowData: data.data.items
+    }));
+  }
+
+  useEffect(() => {
+    getData();
+  }, [
+    tableState.pageSize, tableState.pageNumber,
+    tableState.sortBy, tableState.filters,
+  ])
 
   const defaultColDef = useMemo(() => {
     return {
       filter: "agTextColumnFilter",
       floatingFilter: true,
-    };
-  }, []);
-
-  function addData(){
-    setRowData((prev) => [...prev, ...data])
-    // const res = gridRef.current.api.applyTransaction({
-    //   add: [oneData],
-    //   addIndex: 1
-    // });
-    // console.log(res)
-  }
-
-  function onGridReady(params){
-// console.log(params)
-//     const dataSource = {
-//       rowCount: undefined,
-//       getRows: (params) => {
-
-//         params.successCallback([oneData, oneData, oneData], 991);
-//         console.log("apr")
-//       }
-//     }
-//     params.api.setGridOption("datasource", dataSource);
-
-  }
-
-  function previousPage(){
-    // Fetch then gridRef.current.api.paginationGoToNextPage();
-  }
-
-  function nextPage(){
-    // Fetch then gridRef.current.api.paginationGoToNextPage();
-  }
-
-  const loadingCellRenderer = useCallback(() => {
-    return {}
-  }, []);
-  const loadingCellRendererParams = useMemo(() => {
-    return {
-      loadingMessage: "One moment please...",
     };
   }, []);
 
@@ -125,29 +70,51 @@ export default function Audit(){
         <CRow xs={{ gutterX: 2, gutterY: 2 }}> 
           <CCol>
             <CInputGroup>
-              <CInputGroupText id="basic-addon1" onClick={addData}>
+              <CInputGroupText id="basic-addon1">
                 <CIcon size="lg" icon={cilSearch} />
               </CInputGroupText>
               <CFormInput placeholder="Global search..." aria-label="Username" aria-describedby="username"/>
             </CInputGroup>
           </CCol>
           <CCol xs="auto">
-            <CFormSelect xs="auto"
+            <CFormSelect
             aria-label="Default select example"
             options={[
-             { label: '15 rows', value: 15 },
-             { label: '30 rows', value: 30 },
-             { label: '45 rows', value: 45 },
+             { label: '15 rows', value: "15" },
+             { label: '30 rows', value: "30" },
+             { label: '45 rows', value: "45" },
             ]}
+            onChange={(e : React.ChangeEvent<HTMLSelectElement>) => {
+                setTableState(prev => ({
+                  ...prev,
+                  pageSize: Number(e.target.value)
+                }))
+            }}
            />
           </CCol>
           <CCol xs="auto" className="">
-            <CButton color="secondary">
+            <CButton 
+              color="secondary"
+              onClick={() => {
+                setTableState(prev => ({
+                  ...prev,
+                  pageNumber: Math.max(prev.pageNumber - 1, 1)
+                }))
+              }}  
+            >
               <CIcon icon={cilCaretLeft} />
-              </CButton>
+            </CButton>
           </CCol>
           <CCol xs="auto">
-            <CButton color="secondary" >
+            <CButton 
+              color="secondary"
+              onClick={() => {
+                setTableState(prev => ({
+                  ...prev,
+                  pageNumber: prev.pageNumber + 1
+                }))
+              }}  
+             >
               <CIcon icon={cilCaretRight} />
             </CButton>
           </CCol>
@@ -163,16 +130,13 @@ export default function Audit(){
       <AgGridReact
         suppressPaginationPanel={true}
         ref={gridRef}
-        rowData={rowData}
+        rowData={tableState.rowData}
         columnDefs={columnDefs}
         defaultColDef={defaultColDef}
-        rowSelection={rowSelection}   
         pagination={true}
-        paginationPageSize={10}
-        paginationPageSizeSelector={[10, 25, 50]}
-        onGridReady={onGridReady}
-        loadingCellRenderer={loadingCellRenderer}
-        loadingCellRendererParams={loadingCellRendererParams}
+        paginationPageSize={tableState.pageSize}
+        paginationPageSizeSelector={[15, 30, 45]}
+        onGridReady={getData}
         />
     </div>
   );
