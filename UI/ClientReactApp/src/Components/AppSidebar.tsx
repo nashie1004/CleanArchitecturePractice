a4bfbfd4a760d351700045b66d964d502409974f
Nash <naocenar@gmail.com>
2024-12-21 01:31:43 +0800

    Add simple logo

--- UI/ClientReactApp/src/Components/AppSidebar.tsx
+++ UI/ClientReactApp/src/Components/AppSidebar.tsx
@@ -3,6 +3,8 @@ import { AppSidebarNav } from "./AppSidebarNav";
 import { useContext, useState } from "react";
 import nav from "../nav";
 import useSidebar from "../Hooks/useSidebar";
+import { cilBurn } from "@coreui/icons";
+import CIcon from "@coreui/icons-react";
 
 export default function AppSidebar() {
     const { visible, setVisible } = useSidebar();
@@ -14,8 +16,12 @@ export default function AppSidebar() {
             visible={visible}
             position="fixed"
         >
-            <CSidebarHeader className="border-bottom">
-                <CSidebarBrand>CoreUI</CSidebarBrand>
+            <CSidebarHeader className="border-bottom d-flex align-items-center justify-content-center">
+                <CIcon icon={cilBurn} size="xl"/>
+                <h5 style={{ letterSpacing: ".1px", paddingLeft: "5px" }} className="mt-2">
+                    TheStrength
+                    <span style={{ color: "#39f"}}> Logs</span>
+                </h5>
             </CSidebarHeader>
             <AppSidebarNav items={nav} />
             <CSidebarFooter className="border-top d-none d-lg-flex">
